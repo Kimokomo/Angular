c2bf720eec8fba352d63f94cc60ba9e9313b571a
kimokomo <karim.yassin@hotmail.com>
2025-06-09 18:00:18 +0100

    Google Client ID (öffentlich zulässig) in environment.ts ausgelagert

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:8080/api',
+  googleClientId: '274943667956-m61snb3jq17mkm381bukgjte70jgn9ik.apps.googleusercontent.com'
+};
