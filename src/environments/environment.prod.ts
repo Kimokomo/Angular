699559572df7596cc39dff62e2eb947ec924f314
kimokomo <karim.yassin@hotmail.com>
2025-06-09 17:26:43 +0100

    Remove environments folder from git tracking

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://slicy.it.com/api',
-  googleClientId: '274943667956-m61snb3jq17mkm381bukgjte70jgn9ik.apps.googleusercontent.com'
-
-};
