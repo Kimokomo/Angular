5e6e9bec9f56495cd5866749c7d911df220360e8
kimokomo <karim.yassin@hotmail.com>
2025-06-11 22:17:37 +0200

    implement reset-password component

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.css'
 })
 export class ResetPasswordComponent {
 
+
+  resetPassForm!: FormGroup
+
+  constructor(private fb: FormBuilder){}
+
+    ngOnInit() {
+    this.resetPassForm = this.fb.group({
+      newPassword: ['']
+    });
+  }
+
+  handleResetPassForm() {
+
+  }
 }
